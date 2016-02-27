1f2925e6f420dfdaa0e5e9156fc7a96d0df92301
dasystems <reece.j.holland@gmail.com>
2016-02-27 20:51:41 +0800

    Update RX.cpp

--- RX.cpp
+++ RX.cpp
@@ -4,8 +4,10 @@
 
 
 bool DAS_CONTROLLER::receive(char received_byte){
-  
+  bool valid_sentence=false;
   switch(received_byte){
     case',':
       running_checksum^=received_byte;
     case'$':
+      sentence_begin=true;
+    
