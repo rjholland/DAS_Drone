5ec3bf27b800fca87cdd85705fd8a68a09a7f79e
dasystems <reece.j.holland@gmail.com>
2016-02-28 10:51:19 +0800

    Update and rename TX to TX.cpp

---
+++ TX.cpp
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+bool transmit(uint8_t data_id,uint16_t rc_data[],unit8_t data_size){
+
+  Serial1.print("$M<");
+  Serial1.print(data_size);
+  transmit_running_xor=data_size;
+  Serial1.print(data_id);
+  transmit_running_xor^=data_id;
+  for(int i=0;i<data_size;i++){
+    Serial1.print(rc_data[i]);
+    transmit_running_xor^=rc_data[i];
+    }
+  Serial1.print(transmit_running_xor);
+  return transmit_successful;
+  }
