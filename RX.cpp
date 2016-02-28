5284972a4106e10aa4f2b401c3392027788c17c3
dasystems <reece.j.holland@gmail.com>
2016-02-28 19:37:22 +0800

    Update RX.cpp

--- RX.cpp
+++ RX.cpp
@@ -5,9 +5,25 @@
 
 bool DAS_CONTROLLER::receive(char received_byte){
   bool valid_sentence=false;
+  if ((sentence_length>3)&&(sentence_length<6)){
+    received_id[sentence_length-3];
+    sentence_length++;
+  }
+  else if(sentence_length==6){
+    retrieve_id_structure();
+  }
+  
+  else{ 
   switch(received_byte){
-    case',':
-      running_checksum^=received_byte;
     case'$':
       sentence_begin=true;
-    
+      sentence_length++;
+    case'M':
+      sentence_length++;
+    case'<':
+      sentence_length++;
+  }
+  
+  bool DAS_CONTROLLER::retrieve_id_structure(){
+    switch()
+  }
