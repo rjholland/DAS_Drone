82137c557496677f34e2c2e0143949a79d3e6c5c
dasystems <reece.j.holland@gmail.com>
2016-02-29 14:31:47 +0800

    Update RX.cpp

--- RX.cpp
+++ RX.cpp
@@ -28,5 +28,15 @@ bool DAS_CONTROLLER::receive(char received_byte){
    uint8_t msp_structure[][]{{MSP_IDENT,7},{MSP_STATUS,11},{MSP_RAW_IMU,18},{MSP_SERVO,32},{MSP_MOTOR,32},{MSP_RC,32},{MSP_RAW_GPS,16},
                           {MSP_COMP_GPS,5},{MSP_ATTITUDE,6},{MSP_ALTITUDE,6},{MSP_ANALOG,7},{MSP_RC_TUNING,7},{MSP_PID,30},{MSP_BOX,20},
                           {MSP_MISC,22},{MSP_MOTOR_PINS,8},{MSP_SERVO_CONF,56}};
+     while (first <= last) {
+      if (msp_structure[middle][0] < id)
+         first = middle + 1;    
+      else if (msp_structure[middle][0] == id) {
+         return (msp_structure[middle+1][1])
+      }
+      else
+         last = middle - 1;
+      middle = (first + last)/2;
+   }
     
   }
