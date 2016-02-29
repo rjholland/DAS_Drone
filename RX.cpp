39d17dbd75c0f14063f98ad64a36335140355fbf
dasystems <reece.j.holland@gmail.com>
2016-02-29 17:20:50 +0800

    Update RX.cpp

--- RX.cpp
+++ RX.cpp
@@ -3,16 +3,20 @@
 
 
 
-bool DAS_CONTROLLER::receive(char received_byte){
+bool DAS_CONTROLLER::receive(char received_byte,uint *parsed_data_bytes){
+  
   bool valid_sentence=false;
-  if ((sentence_length>3)&&(sentence_length<6)){
+  if ((sentence_length>5)&&(sentence_length<(6+data_length)))
+    parsed_data_bytes[sentence_length-6]=received_byte;
+    sentence_length++;
+  else if ((sentence_length>3)&&(sentence_length<6)){
     received_id[sentence_length-3];
     sentence_length++;
   }
   else if(sentence_length==6){
-    retrieve_id_structure(atoi(received_id));
+    data_length=retrieve_id_structure(atoi(received_id))
+    *parsed_data_bytes=malloc(data_length);
   }
-  
   else{ 
   switch(received_byte){
     case'$':
