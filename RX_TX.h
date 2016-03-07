e3bc972e4622dde603d160b299675fd4d705d561
dasystems <reece.j.holland@gmail.com>
2016-03-07 19:33:04 +0800

    Added files via upload

---
+++ RX_TX.h
@@ -0,0 +1,84 @@
+
+
+#ifndef RX_TX.h
+#define RX_TX.h
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+//From flight controller
+#define MSP_IDENT 100,
+#define MSP_STATUS 101
+#define MSP_RAW_IMU 102
+#define MSP_SERVO 103
+#define MSP_MOTOR 104
+#define MSP_RC 105
+#define MSP_RAW_GPS 106
+#define MSP_COMP_GPS 107
+#define MSP_ATTITUDE 108
+#define MSP_ALTITUDE 109
+#define MSP_ANALOG 110
+#define MSP_RC_TUNING 111
+#define MSP_PID 112
+#define MSP_BOX 113
+#define MSP_MISC 114
+#define MSP_MOTOR_PINS 115
+#define MSP_BOXNAMES 116
+#define MSP_PIDNAMES 117
+#define MSP_WP 118
+#define MSP_SERVO_CONF 120
+#define MSP_BOXIDS 119
+
+
+//To flight controller
+#define MSP_SET_MOTOR 214
+#define MSP_SET_RAW_RC 200
+#define MSP_SET_RAW_GPS 201
+#define MSP_SET_RC_TUNING 204
+#define MSP_SET_PID 202
+#define MSP_SET_BOX 203
+#define MSP_SET_MISC 207
+#define MSP_SET_SERVO_CONF 212
+#define MSP_ACC_CALIBRATION 205
+#define MSP_MAG_CALIBRATION 206
+#define MSP_RESET_CONF 208
+#define MSP_SELECT_SETTING 210
+#define MSP_SET_HEAD 211
+#define MSP_BIND 240
+#define MSP_EEPROM_WRITE 250
+
+class DAS_CONTROLLER{
+  private:
+    uint8_t running_checksum;
+    bool sentence_begin;
+    bool transmit_successful;
+    uint8_t transmit_running_xor;
+    uint8_t sentence_length;
+    char received_id[4];
+    char received_length[4];
+    uint8_t first;
+    uint8_t last;
+    uint8_t middle;
+    uint8_t n;
+    uint8_t data_length;
+    uint8_t receive_running_xor;
+    uint8_t received_data_bytes;
+    char CS[4];
+    
+};
+bool DAS_CONTROLLER::receive(char received_byte,uint16_t *parsed_data_bytes);
+bool DAS_CONTROLLER::transmit(uint8_t data_id,uint16_t send_data[],uint8_t data_size);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ drone_controller.ino
@@ -0,0 +1,72 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+#include <RX_TX.h>
+#include <math_func.h>
+
+
+struct SEND_DATA_STRUCTURE{
+uint16_t raw_rc[8];
+};
+
+SEND_DATA_STRUCTURE stream;
+
+int frequency = 30;
+bool rc_active=false;
+
+#define virbrationMotorPin 2
+#define control_factor 3
+#define zero 800
+#define full 2000
+#define neutral 1500
+#define mid_upper_range 523
+#define max_range 1023
+#define mid_lower_range 500
+#define max_height_dif 100
+#define max_height_dif_thrust 100
+#define height_lock_factor 0
+#define array_length 16
+#define message_id 200
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  InitIO();             //Initialize the inputs/outputs and the buffers
+ 
+}
+
+void InitIO() {
+  for (int i = 0; i < 5; i++) pinMode(i, INPUT);
+  pinMode(virbrationMotorPin, OUTPUT);
+  digitalWrite(virbrationMotorPin, LOW); // Stop shacking of the gamepad
+}
+
+uint32_t timer = 0;
+
+void loop()
+{
+  if (millis() - timer > frequency) { // manage the updating freq of all the controlling information
+    transmit(selected_id,stream.raw_rc,length_in_bytes);  //read the buttons and the joysticks data
+    DataUpdate();
+    timer = millis();
+  }
+}
+     
+void DataUpdate() {
+ for (int i = 0; i < 4; i++)
+    stream.raw_rc[i] = control_mapper(analogRead(i + 2),control_factor);
+}
+
+uint16_t control_mapper(uint16_t signal,uint8_t factor){
+ uint16_t augmented_signal;
+ if (signal < mid_lower_range)
+   augmented_signal=fscale(0,mid_lower_range,1000,1500,signal,(factor*-1));
+ else if (signal > mid_upper_range)
+   augmented_signal=fscale(mid_upper_range,max_range,1500,2000,signal,factor);
+ else
+   augmented_signal=neutral;
+ return(augmented_signal);
+}
